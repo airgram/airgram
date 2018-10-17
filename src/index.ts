3c4821038f530e080970188815ff93feded625c2
esindger <esindger@gmail.com>
2018-10-17 11:57:13 +0800

    Export telegram types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import Airgram from './Airgram'
+import * as api from './api'
 import TYPES from './ioc/types'
 
 export default Airgram
 export * from './components'
 export * from './config'
 export * from './interfaces'
-export { Airgram, TYPES }
+export { Airgram, api, TYPES }
